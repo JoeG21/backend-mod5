e734d29f443a3427fdf71f16d75f3f21bb46fe27
JoeG21 <jcggomez@live.com>
2020-10-21 16:32:43 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,61 +50,61 @@ s1 = Shelter.create({
 s2 = Shelter.create({
     name: 'Star of Hope - Women & Family Emergency Shelter',
     location: '419 Dowling Street, Houston, TX 77003',
-    img: 'hello world',
+    img: 'https://www.pieperhouston.com/wp-content/uploads/2018/10/SOHFront.jpg',
     phone: 7132222220
 })
 s3 = Shelter.create({
     name: 'Citizens For Animal Protection',
     location: '107 E 22nd St, Houston, TX 77008',
-    img: 'hello world',
+    img: 'https://cultrissecurity.com/wp-content/uploads/2018/07/Citizens-for-Animal-Protetion-800x341.jpg',
     phone: 2814970591
 })
 s4 = Shelter.create({
     name: 'Friends For Life',
     location: '17555 Katy Fwy, Houston, TX 77094',
-    img: 'hello world',
+    img: 'https://inhabitat.com/wp-content/blogs.dir/1/files/2013/05/Friends-For-Life-Gensler-12.jpg',
     phone: 7138639835
 })
 s5 = Shelter.create({
     name: 'Volunteer Houston',
     location: '3303 Main St, Houston, TX 77002',
-    img: 'hello world',
+    img: 'https://www.clipartkey.com/mpngs/m/35-355495_icon-of-office-building-building-png-cartoon.png',
     phone: 2816561533
 })
 s6 = Shelter.create({
     name: 'Loaves & Fishes Soup Kitchen (Magnificat Houses)',
     location: '2009 Congress St, Houston, TX 77002',
-    img: 'hello world',
+    img: 'https://images1.houstonpress.com/imager/u/745xauto/9588643/hou_news_i-45_dsc_6661_photo_by_jeff_balke.jpg',
     phone: 7135294231
 })
 s7 = Shelter.create({
     name: 'Houston SPCA',
     location: '7007 Old Katy Rd, Houston, TX 77024',
-    img: 'hello world',
+    img: 'https://jwcouchfoundation.org/assets/stories/houston_spca_7.jpg',
     phone: 7138697722
 })
 s8 = Shelter.create({
     name: 'The Beacon',
     location: '1212 Prairie St, Houston, TX 77002',
-    img: 'hello world',
+    img: 'https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1365,w_2048,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1200/thebeacon_Facebook_jpu85z.jpg',
     phone: 7132209737
 })
 s9 = Shelter.create({
     name: 'Volunteers of America Texas',
     location: '4808 Yale St, Houston, TX 77018',
-    img: 'hello world',
+    img: 'https://www.clipartkey.com/mpngs/m/35-355495_icon-of-office-building-building-png-cartoon.png',
     phone: 7134600781
 })
 s10 = Shelter.create({
     name: 'Habitat for Humanity Northwest Harris County',
     location: '13350 Jones Rd, Houston, TX 77070',
-    img: 'hello world',
+    img: 'https://s.hdnux.com/photos/53/11/41/11311193/9/1200x0.jpg',
     phone: 2818905585
 })
 s11 = Shelter.create({
     name: 'Houston Humane Society',
     location: '14700 Almeda Rd, Houston, TX 77053',
-    img: 'hello world',
+    img: 'https://www.houstonhumane.org/Themes/Default/Content/Assets/Location%20and%20Hours.jpg',
     phone: 7134336421
 })
 
